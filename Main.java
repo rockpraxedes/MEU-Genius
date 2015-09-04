62a68b32593e8cb91179f7f9ca0688b75a167042
Sérgio Praxedes <rock.praxedes@yahoo.com.br>
2015-09-04 10:19:05 -0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,17 @@
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.*;
+import java.io.*;
+
+
+
+public class Main{	
+
+	public static void main(String[] args) {
+		
+		Painel Genius = new Painel ();
+		
+
+	}
+}	
