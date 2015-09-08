aa16a9ac161cf029e80fb0cb700f4c6298feb288
Sérgio Praxedes <rock.praxedes@yahoo.com.br>
2015-09-08 10:54:16 -0300

    AJUDAAA
    Não sei como faço essa classe pisca, e como chamo ela, setada para cada botao, ou seja, como chamo ela ja selecionando o Switch.(1,2,3,4) =x
    
    VICTORR!!!

--- pisca.java
+++ pisca.java
@@ -9,8 +9,24 @@ import java.io.*;
  public class pisca extends JButton{
 	
 	public int op;
+	private botao[][] bot_jogo = new botao[6][6];
+	private int tempo = 250;
+	Icon b1 = new ImageIcon("b1.gif");  
+	Icon b2 = new ImageIcon("b2.gif");  
+	Icon b3 = new ImageIcon("b3.gif");  
+	Icon b4 = new ImageIcon("b4.gif");  
+	Icon pb1 = new ImageIcon("pb1.gif");  
+	Icon pb2 = new ImageIcon("pb2.gif");  
+	Icon pb3 = new ImageIcon("pb3.gif");  
+	Icon pb4 = new ImageIcon("pb4.gif");  
+		
+	public void setOp(int op){
+		this.op = op;
+	}	
+	
 	
 	public pisca(){
+
 		switch(op){
 			case 1:{
 					bot_jogo [1][1].setIcon(pb1); 
